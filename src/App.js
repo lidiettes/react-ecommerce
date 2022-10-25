0e3c9d37f009cd99ee361a7cdaa40e9d17cdd1d1
lidiettes <hello@lidiettes.com>
2022-10-25 23:27:49 +0200

    changes nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-  <h1>hola que tal</h1>
+  <Header />
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import './Header.css';
+
+const Header = () => {
+return (
+    <>
+        <header>
+		<div className="wrapper">
+			<div className="logo">Soy un header to tocho</div>
+			
+			<nav>
+				<a href="#">Home</a>
+				<a href="#">Shop</a>
+				<a href="#">About us</a>
+				<a href="#">Contact</a>
+			</nav>
+		</div>
+	</header>
+
+    </>
+)
+};
+
+export default Header;
+
+
