ff0e74fbc2361bcbaafb33e3c8ea45f869a256ab
lidiettes <hello@lidiettes.com>
2022-10-26 17:43:42 +0200

    productContainer created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import ProductsContainer from './components/ProductsContainer/ProductsContainer';
 
 function App() {
   return (
   <>
   <Header />
+  <ProductsContainer/>
   <Footer />
   </>
   );
