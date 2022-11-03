a428d3806576e6d0f35eb4d2af269855e6822091
lidiettes <hello@lidiettes.com>
2022-11-03 13:42:13 +0100

    pruebas

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+import Layout from './routers/Layout';
+
+
+
+function App() {
+
+
+
+
+  return (
+    <>
+
+    <Layout/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/CartFinal.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import CartNav from './components/CartNav/CartNav'
+
+const CartFinal = () => {
+  return (
+    <div>
+        <ul>
+            <li>lista de articulos</li>
+            <CartNav />
+
+
+
+        </ul>
+    </div>
+  )
+}
+
+export default CartFinal
