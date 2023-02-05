ff577e5798783f621411f9e1ee17f24318277d71
lidiettes <hello@lidiettes.com>
2023-02-05 13:33:53 +0100

    deleted commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
 import Router from './routes/Router';
+
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import GeneralProvider from "./context/GeneralContext.jsx/GeneralProvider";
 import Helper from "./components/Helper/Helper";
-
-
 
 
 
